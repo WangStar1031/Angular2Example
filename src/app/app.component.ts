d48cf69c306e05ccc7dadda818185a9f1c72f77d
WangXing <wangstar1031@hotmail.com>
2017-08-17 11:50:05 -0700

    Third

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
 import { Component } from '@angular/core';
+//import { PeopleService } from './people.service';
+//import { IProduct } from './products';
+//import { ProductService } from './products.service';
+import { appService} from './app.service';
+
+import { Http , Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ appService]
 })
 export class AppComponent {
-  title = 'My First App';
+//  iproducts: IProduct[];
+  value: string = "";
+  appTitle: string = 'welcome';
+  appList: any[] = [{
+    "ID":"1",
+    "Name":"One",
+    "url":'app/Image/One.jpg'
+  },
+  {
+    "ID":"2",
+    "Name":"Two",
+    "url":'app/Image/two.jpg'
+  }];
+  constructor( private _appService: appService ){
+
+  }
+  ngOnInit() : void{
+    this.value = this._appService.getApp();
+  //  this._product.getproducts().subscribe(iproducts => this.iproducts = iproducts);
+  }
+  title = 'My First Project';
+  size = 150;
+  oneClicked = false;
+  imageSize(){
+  	return this.size * 2;
+  };
+  Clicked(){
+  	this.oneClicked = !this.oneClicked;
+  	//alert("123 p-tag Clicked!");
+  }
 }
